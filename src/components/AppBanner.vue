11cbfc29cd33e2461f278733ac2fff5745767199
StefanoM98 <smartino030@gmail.com>
2023-04-12 13:20:59 +0200

    Add: my first app with vite and bonus Banner

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: "AppBanner",
+    data() {
+      return {
+        title: "Benvenuto nella mia app"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <img src="../assets/vue.svg" alt="">
+</template>
+
+<style>
+
+</style>
